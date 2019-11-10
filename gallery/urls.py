5acd05feafdb410b6d69cf939b9dbd073bb47382
AliKheir <akheirali@gmail.com>
2019-11-10 10:47:34 +0300

    created a photos url pattern in urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import photos.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^photos/', photos.views.ali, name='ali'),
+
 ]
