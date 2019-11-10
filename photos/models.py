a8fd22e23a89a9886f1e248e47ee2a58609fff8f
AliKheir <akheirali@gmail.com>
2019-11-10 14:46:42 +0300

    created the image name, description, category and location models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=20)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self):
+        self.update_location()
+
+    @classmethod
+    def get_location(cls):
+        place = cls.objects.all()
+        return place
+
+    def __str__(self):
+        return self.location
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=20)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self):
+        self.update_category()
+
+    def __str__(self):
+        return self.category
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField()
+    img_location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.image_name
+
+    class Meta:
+        ordering = ['-image']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        self.update_image()
+
+    @classmethod
+    def get_images(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        img_id = cls.objects.get(pk=id)
+        return img_id
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        images = cls.objects.filter(category__category__icontains=search_term)
+        return images
+
