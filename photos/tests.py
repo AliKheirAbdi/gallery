f9453beac55f97f8be4dbdcd3b5300758e63ba81
AliKheir <akheirali@gmail.com>
2019-11-11 13:23:15 +0300

    added image test

--- photos/tests.py
+++ photos/tests.py
@@ -1,28 +1,55 @@
 from django.test import TestCase
-from .models import Image, Location, Category
+
+from .models import Image, Category, Location
 
 
-# Create your tests here.
-class ImageTestClass(TestCase):
-    """
-    Test class image that tests for the image class behaviour
-    """
-
+class TestImage(TestCase):
     def setUp(self):
-        pass
+        self.location = Location(name='Nairobi')
+        self.location.save_location()
 
+        self.category = Category(name='travel')
+        self.category.save_category()
 
-class LocationTestClass(TestCase):
-    """
-    tests for the behaviour of location class
-    """
-    def setUp(self):
-        pass
+        self.image_test = Image(id=1, name='image', description='my photo', location=self.location,
+                                category=self.category)
 
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
 
-class CategoryTestClass(TestCase):
-    """
-    tests for the behaviour of category class
-    """
-    def setUp(self):
-        pass
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def test_update_image(self):
+        self.image_test.save_image()
+        self.image_test.update_image(self.image_test.id, 'photos/ali.jpg')
+        changed_img = Image.objects.filter(image='photos/ali.jpg')
+        self.assertTrue(len(changed_img) > 0)
+
+    def test_get_image_by_id(self):
+        found_image = self.image_test.get_image_by_id(self.image_test.id)
+        image = Image.objects.filter(id=self.image_test.id)
+        self.assertTrue(found_image, image)
+
+    def test_search_image_by_location(self):
+        self.image_test.save_image()
+        found_images = self.image_test.filter_by_location(location='Nairobi')
+        self.assertTrue(len(found_images) == 1)
+
+    def test_search_image_by_category(self):
+        category = 'travel'
+        found_img = self.image_test.search_by_category(category)
+        self.assertTrue(len(found_img) > 1)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+
